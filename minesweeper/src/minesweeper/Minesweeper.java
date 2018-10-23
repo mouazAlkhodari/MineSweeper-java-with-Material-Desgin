48091fc3872d171d08c99646ff23a5430d83c1e1
Omar <example@git.com>
2018-10-23 12:32:17 +0300

    Testing

--- minesweeper/src/minesweeper/Minesweeper.java
+++ minesweeper/src/minesweeper/Minesweeper.java
@@ -16,6 +16,7 @@ public class Minesweeper {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello guys Engo using git with me ");
     }
     
 }

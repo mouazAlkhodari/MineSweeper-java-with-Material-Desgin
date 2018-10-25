6bded0669ca6baff1ceedda5e37a7b277b41d35e
Da <Da>
2018-10-25 10:57:19 +0300

    Changed SquareSatuts from class to simple enum .. And deleted some unused lines in Mine class

--- minesweeper/src/minesweeper/Mine.java
+++ minesweeper/src/minesweeper/Mine.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package minesweeper;
 
-/**
- *
- * @author Da
- */
 public class Mine {
    int ID;
    //to define unique id for each mine

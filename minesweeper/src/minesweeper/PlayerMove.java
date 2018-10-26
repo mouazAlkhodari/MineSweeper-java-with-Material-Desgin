8e6c630c48f1bead0797a78525eede6ff04b701d
Da <Da>
2018-10-26 16:34:38 +0300

    removed error in playermove

--- minesweeper/src/minesweeper/PlayerMove.java
+++ minesweeper/src/minesweeper/PlayerMove.java
@@ -6,12 +6,11 @@ public class PlayerMove {
     Square square;
     MoveType type;
     MoveResult result;
-<<<<<<< HEAD
-}   
-=======
 
     public PlayerMove(Player _player,Square _square,MoveType _type,MoveResult _result) {
-        player=_player;square=_square;type=_type;result=_result;
+        this.player= _player;
+        this.square=_square;
+        this.type=_type;
+        this.result=_result;
     }
 }
->>>>>>> ef7725448ae480293566145dce5f111d59cf1dbe

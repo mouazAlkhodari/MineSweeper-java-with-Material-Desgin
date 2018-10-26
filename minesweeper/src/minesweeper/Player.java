092598b7e16612254b8bbff5a5c27d6207e47313
mouaz <mouazx19999@gmail.com>
2018-10-26 17:20:29 +0200

    make everythings Private :3

--- minesweeper/src/minesweeper/Player.java
+++ minesweeper/src/minesweeper/Player.java
@@ -1,8 +1,8 @@
 package minesweeper;
 
 public abstract class Player {
-    String name;
-    int currentScore;
+    private String name;
+    private int currentScore;
 
     public Player(String _name,int _currentScore){
         name=_name;

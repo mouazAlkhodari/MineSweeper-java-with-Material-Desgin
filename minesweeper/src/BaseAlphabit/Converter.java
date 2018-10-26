d3bf5314d571be0417da4dc57f4e3e38d885b8d7
mouaz <mouazx19999@gmail.com>
2018-10-26 18:30:20 +0200

    add BaseAlphabit Package

---
+++ minesweeper/src/BaseAlphabit/Converter.java
@@ -0,0 +1,19 @@
+package BaseAlphabit;
+
+public class Converter {
+    public static int valueOf(String alphaNum) {// get integer value of number in alphabet base number...
+        int ret=0;
+        for(int i=0;i<alphaNum.length();i++){
+            ret=ret*26+(Integer.valueOf(alphaNum.charAt(i))-(int)'a'+1);
+        }
+        return ret;
+    }
+    public static String valueOf(int num) {// get integer value of number in alphabet base number...
+        String ret="";
+        while(num!=0){
+            ret+='a' + (num%26);
+            num/=26;
+        }
+        return ret;
+    }
+}

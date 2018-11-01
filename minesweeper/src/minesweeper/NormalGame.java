5ecf3160847ab7f8bcb53f767f0995d484a7d751
mouaz <mouazx19999@gmail.com>
2018-11-01 13:17:08 +0200

    implement Change Status And DecideNext Player Func

--- minesweeper/src/minesweeper/NormalGame.java
+++ minesweeper/src/minesweeper/NormalGame.java
@@ -12,6 +12,14 @@ public abstract class NormalGame extends Game{
             return 0;
         }
         Player DecideNextPlayer(ArrayList moves){
+            int indOfcurrentPlayer=players.lastIndexOf(currentPlayer);
+            for(int idx=indOfcurrentPlayer,i=0;i<players.size();i++){
+                idx+=(idx+1)%players.size();
+                if(players.get(idx).getCurrentStatus()!=PlayerStatus.Lose){
+                    return players.get(i);
+                }
+            }
+            return currentPlayer;
         }
     }
 
@@ -19,10 +27,45 @@ public abstract class NormalGame extends Game{
     public void ApplyPlayerMove(PlayerMove move) {
         // here We ApPly The move And then Check The Status Of The Game
         moves =this.grid.AcceptMove(move);
-        Square[][] feild=this.grid.getField();
+
         int ScoreChange=currentRules.GetScoreChange(moves);
         currentPlayer.addScore(ScoreChange);
+        // need To be Func
+            if(moves.get(0).getSquare().getStatus()==SquareStatus.OpenedMine) {
+                if (LoseWhenHitMine) {
+                    currentPlayer.setCurrentStatus(PlayerStatus.Lose);
+                } else if (currentPlayer.getCurrentScore() < 0) {
+                    currentPlayer.setCurrentStatus(PlayerStatus.Lose);
+                }
+            }
+        ChangeStatus();
         currentPlayer=currentRules.DecideNextPlayer(moves);
 
+    }
+    public void ChangeStatus(){
+        Square[][] feild =this.grid.getField();
+        int num=0;
+        for(int i=1;i<this.grid.getWidth();i++){
+            for(int j=1;j<this.grid.getHeight();j++){
+                switch (feild[i][j].getStatus()){
+                    case Marked:
+                    case Closed:
+                        num++;
+                        break;
+                }
+            }
+        }
+        boolean CanContinue=false;
+        for(int i=0;i<players.size();i++){
+            if(players.get(i).getCurrentStatus()!=PlayerStatus.Lose){
+                CanContinue=true;
+            }
+        }
+        if(num==this.grid.getMinesCount() || !CanContinue){
+            status=GameStatus.Finish;
+        }
+        else{
+            status=GameStatus.Running;
+        }
     }
 }

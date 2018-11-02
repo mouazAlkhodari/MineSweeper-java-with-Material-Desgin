be736c7ec7638127d979118cb8c88406ac7bdf94
Da <Da>
2018-11-02 15:39:59 +0300

    Fixing errors

--- minesweeper/src/minesweeper/Minesweeper.java
+++ minesweeper/src/minesweeper/Minesweeper.java
@@ -8,12 +8,9 @@ package minesweeper;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import java.util.Vector;
 
 /**
  *

673a4766412ff3233fd1e4857d7a8e53f2b71112
Da <Da>
2018-11-15 23:43:30 +0300

    edit Shield

--- minesweeper/src/Models/Grid/Grid.java
+++ minesweeper/src/Models/Grid/Grid.java
@@ -46,7 +46,6 @@ public class Grid {
         MineSweeperGrid generatedGrid = new MineSweeperGrid(width, height,minesCount,shieldsCount,heroShieldsCount,move);
         SurroundingMinesMatrix numberOfSurroundedmines = new SurroundingMinesMatrix(width, height, generatedGrid);
 
-
         //init sqaures inside the field
         for (int i = 1 ;i < height; i++) {
             for (int j = 1;j < width; j++) {

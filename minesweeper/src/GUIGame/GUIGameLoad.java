325775b3a83d717fd72b8810810b72d6ba069ca8
mouaz <mouazx19999@gmail.com>
2018-12-07 16:12:05 +0200

    make Game load right

--- minesweeper/src/GUIGame/GUIGameLoad.java
+++ minesweeper/src/GUIGame/GUIGameLoad.java
@@ -26,7 +26,7 @@ public class GUIGameLoad {
         scene=new Scene(layout);
     }
     public void addGame(String game){
-        games.getItems().add(0,game);
+        games.getItems().add(0,Directories.getVal(game));
     }
     private void initLayout(){
         // load The List
